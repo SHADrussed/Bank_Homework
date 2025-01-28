bdc1997a9b0e5875e49a46e0903ab4c078470c48
SHADrussed <shad_rus_teaT@mail.ru>
2025-01-28 18:24:29 +0300

    1.0 git use

--- tests/example.py
+++ tests/example.py
@@ -1,4 +1,5 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
-print(get_mask_card_number(int(input())))
-print(get_mask_account(int(input())))
+print(get_date('2024-03-11T02:26:18.671407'))
+print(mask_account_card('Visa Platinum 7000792289606361'))
+print(mask_account_card('Счет 73654108430135874305'))
