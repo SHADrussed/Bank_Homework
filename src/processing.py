9c4cd7007fc648df515b3554ee00ecc18ca5bbf3
SHADrussed <shad_rus_teaT@mail.ru>
2025-02-07 19:41:10 +0300

    made sort_by_date and a test for it

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,15 @@
-def filter_by_state(list_of_kwargs, state = 'EXECUTED'):
-    new_list_kwargs = []
+from typing import Any
+
+
+def filter_by_state(list_of_kwargs, state='EXECUTED'):
+    """Сортирует список по статусу"""
+    new_list_kwargs: list[Any] = []
     for i in list_of_kwargs:
         if i['state'] == state:
             new_list_kwargs.append(i)
     return new_list_kwargs
+
+
+def sort_by_date(operations, reverse=True):
+    """Сортирует список словарей по дате."""
+    return sorted(operations, key=lambda x: x['date'], reverse=reverse)
