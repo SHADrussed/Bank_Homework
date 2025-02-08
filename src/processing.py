313b998dd709a3aebf00876ad2756c3a681ef3da
SHADrussed <shad_rus_teaT@mail.ru>
2025-02-08 13:08:49 +0300

    Made functions to the homework expectations and added a README.md file

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,12 @@
-from typing import Any
-
-
-def filter_by_state(list_of_kwargs, state='EXECUTED'):
+def filter_by_state(list_of_kwargs: list[dict[str, object]], state: str = "EXECUTED") -> list:
     """Сортирует список по статусу"""
-    new_list_kwargs: list[Any] = []
+    new_list_kwargs: list = []
     for i in list_of_kwargs:
-        if i['state'] == state:
+        if i["state"] == state:
             new_list_kwargs.append(i)
     return new_list_kwargs
 
 
-def sort_by_date(operations, reverse=True):
+def sort_by_date(operations: list, reverse: bool = True) -> list:
     """Сортирует список словарей по дате."""
-    return sorted(operations, key=lambda x: x['date'], reverse=reverse)
+    return sorted(operations, key=lambda x: x["date"], reverse=reverse)
